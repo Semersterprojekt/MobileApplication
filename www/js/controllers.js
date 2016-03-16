be3e072546d1c5a35942a4bd771c7f4a0cfe3095
getspeed <danilo89ds@gmail.com>
2016-03-16 18:11:59 +0100

    Kamerafunktion funktioniert. Upload confirm / deny.
    Upload zum Server funktioniert

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-var app = angular.module('starter.controllers', []);
+var app = angular.module('starter.controllers', ['ionic']);
 
 app.controller('DashCtrl', function ($scope) {
 });
@@ -29,7 +29,70 @@ app.controller('AccountCtrl', function ($scope) {
 });
 
 
-app.controller('CameraCtrl', function ($scope) {
+app.controller('CameraCtrl', function ($scope, $cordovaCamera, $http, $ionicPopup) {
 
   $scope.pictureUrl = 'http://placehold.it/300x300';
-})
+
+  //diese Funktion startet die Kamera.
+  $scope.takePicture = function () {
+
+    //Optionen Für die Kamera
+    var options = {
+      quality: 100,
+      destinationType: Camera.DestinationType.DATA_URL,
+      sourceType: Camera.PictureSourceType.CAMERA,
+      allowEdit: false,
+      encodingType: Camera.EncodingType.JPEG,
+      targetWidth: 200,
+      targetHeight: 200,
+      popoverOptions: CameraPopoverOptions,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    };
+
+    $cordovaCamera.getPicture(options).then(function (imageData) {
+
+      $scope.pictureUrl = "data:image/jpeg;base64," + imageData;
+      sendPhoto(imageData);
+    }, function (err) {
+      // error
+    });
+
+  }
+
+  function sendPhoto(image) {
+
+    var url = "http://193.5.58.95/api/v1/tests";
+
+    var data = "Hier stehen die Daten drin";
+    var comment = "Hier steht der Kommentar drin";
+    var b64 = image;
+
+    var output = {
+      data: data,
+      comment: comment,
+      base64: b64
+    }
+
+    var confirmPopup = $ionicPopup.confirm({
+      title: 'Nice Picture',
+      template: 'Do you want to upload this Picture?'
+    });
+
+
+    confirmPopup.then(function (res) {
+      if (res) {
+        $http.post(url, output, {headers: {'Content-Type': 'application/json'}})
+          .then(function (response) {
+          });
+      } else {
+        //der user hat das Bild verworfen.
+        $scope.pictureUrl = 'http://placehold.it/300x300';
+      }
+    });
+
+
+  };
+
+
+});
